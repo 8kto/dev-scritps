0f8b97bcee48ee27bc9e966de1956393df767387
Okto <web@axisful.info>
2019-10-28 16:07:26 +0100

    Update dump db script

--- bash/dump-db.sh
+++ bash/dump-db.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #==============================================================================
-# Dump database. Script should be launched only within Docker container as it
-# contains actual project database.
+# Dump database.
 # Saves files into `var/dump/` directory.
+# WARNING: all `mysqldumps` calls should be configured with the actual params
 #
-# Author: Igor Ivlev <ivlev@kurzor.net>
+# Author: Igor Ivlev <ivlev@ifhtagn@gmail.com>
 #
 # Usage:
 #       ./scripts/dump-db.sh OPTIONS
@@ -26,8 +26,10 @@
 #
 #==============================================================================
 
-CURDIR=`/bin/pwd`
-BASEDIR=$(dirname $0)
+BASEDIR=$(dirname "$0")
+
+# Include lib
+source "${BASEDIR}"/lib.sh
 
 # Pick up script arguments
 _RAW_SQL=true
@@ -36,140 +38,126 @@ _USE_DATESTAMP=false
 _BACKUP_DIR=${CURDIR}/var/dump
 _NONINTERACTIVE=false
 
-# Include lib
-source ${BASEDIR}/lib.sh
-
 # Check if all the utils are installed
-_EXECUTABLES=( mysqldump gzip )
-for bin in "${_EXECUTABLES[@]}" ; do
-	if [[ -z "$(which ${bin})" ]] ; then
-	    error "Not found executable for ${bin}"
-	    exit 1
-	fi
+_EXECUTABLES=(mysqldump gzip)
+for bin in "${_EXECUTABLES[@]}"; do
+  if [[ -z "$(command -v "${bin}")" ]]; then
+    echo "Not found executable for ${bin}"
+    exit 1
+  fi
 done
 
 # Generate name for dump file
-function generate_filename {
-    local ext_suffix=$1
-    local suffix
+function generate_filename() {
+  local ext_suffix=$1
+  local suffix
 
-    if [[ ${_USE_DATESTAMP} = true ]] ; then
-        suffix=-$(date +%Y-%m-%d.%H%M%S)
-    else
-        suffix='-latest'
-    fi
+  if [[ ${_USE_DATESTAMP} == true ]]; then
+    suffix=-$(date +%Y-%m-%d.%H%M%S)
+  else
+    suffix='-latest'
+  fi
 
-    result=${_BACKUP_DIR}/database-dump${suffix}.sql${ext_suffix}
-    echo ${result}
+  echo "${_BACKUP_DIR}"/database-dump${suffix}.sql"${ext_suffix}"
 }
 
 # Script options
-for arg in "$@" ; do
-    case ${arg} in
-        --raw-sql)
-            _RAW_SQL=true
-            _USE_GZIP=false
-            shift
-        ;;
-        --use-gzip)
-            _RAW_SQL=false
-            _USE_GZIP=true
-            shift
-        ;;
-        --use-datestamp)
-            _USE_DATESTAMP=true
-            shift
-        ;;
-        --output)
-            _BACKUP_DIR=$2
-            shift 2
-        ;;
-        --none-interactive|-n)
-            _NONINTERACTIVE=true
-            shift
-        ;;
-        --help|-h)
-            show_help 26
-            exit 0
-        ;;
-    esac
+for arg in "$@"; do
+  case ${arg} in
+    --raw-sql)
+      _RAW_SQL=true
+      _USE_GZIP=false
+      shift
+      ;;
+    --use-gzip)
+      _RAW_SQL=false
+      _USE_GZIP=true
+      shift
+      ;;
+    --use-datestamp)
+      _USE_DATESTAMP=true
+      shift
+      ;;
+    --output)
+      _BACKUP_DIR=$2
+      shift 2
+      ;;
+    --none-interactive | -n)
+      _NONINTERACTIVE=true
+      shift
+      ;;
+    --help | -h)
+      show_help 26
+      exit 0
+      ;;
+  esac
 done
 
 # Check if dump dir is exists on current path
 if [[ ! -d ${_BACKUP_DIR} ]]; then
-    cd ../
+  cd ../
 fi
 
 if [[ ! -d ${_BACKUP_DIR} ]]; then
-    error 'Cannot find dump dir'
-    exit 1
+  error 'Cannot find dump dir'
+  exit 1
 fi
 
 # Ask user
-if [[ ${_NONINTERACTIVE} = false ]] ; then
-    while true ; do
-        read -p "Do you wish to dump [dev] database into ${_BACKUP_DIR}? (Y/N) " U_INPUT
+if [[ ${_NONINTERACTIVE} == false ]]; then
+  while true; do
+    read -r -p "Do you wish to dump [dev] database into ${_BACKUP_DIR}? (Y/N) " U_INPUT
 
-        case ${U_INPUT} in
-            [Yy]*)
-                break
-             ;;
-            [Nn]*)
-                exit 0
-            ;;
-            *)
-                error "Please answer (y) or (n)."
-             ;;
-        esac
-    done
+    case ${U_INPUT} in
+      [Yy]*)
+        break
+        ;;
+      [Nn]*)
+        exit 0
+        ;;
+      *)
+        error "Please answer (y) or (n)."
+        ;;
+    esac
+  done
 fi
 
 print 'Start dumping...'
 
-# Clear caches, logs, non-needed data objects +
-# internal Pimcore maintenance jobs
-print "Clear non-needed stuff..."
-./bin/console pimcore:email:cleanup --older-than-days 0
-./bin/console cache:clear
-./bin/console pimcore:cache:clear
-./bin/console pipopr:submissions:clear
-./bin/console pimcore:maintenance -f -v
+if [[ ${_RAW_SQL} == true ]]; then
+  print 'Dump raw sql...'
 
-if [[ ${_RAW_SQL} = true ]] ; then
-    print 'Dump raw sql...'
+  filepath=$(generate_filename)
+  mysqldump > "${filepath}"
 
-    filepath=$(generate_filename)
-    mysqldump $(get_db_info 'mysqldump') > ${filepath}
+  if [[ $? != 0 ]]; then
+    error 'Dump failed'
+    exit 1
+  fi
+elif [[ ${_USE_GZIP} == true ]]; then
+  print 'Dump and compress sql...'
 
-    if [[ $? != 0 ]] ; then
-        error 'Dump failed: had been launched within Docker?'
-        exit 1
-    fi
-elif [[ ${_USE_GZIP} = true ]] ; then
-    print 'Dump and compress sql...'
+  filepath=$(generate_filename '.gz')
+  mysqldump | gzip > "${filepath}"
 
-    filepath=$(generate_filename '.gz')
-    mysqldump $(get_db_info) | gzip > ${filepath}
-
-    if [[ $? != 0 ]]; then
-        error 'Dump failed: had been launched within Docker?'
-        exit 1
-    fi
+  if [[ $? != 0 ]]; then
+    error 'Dump failed'
+    exit 1
+  fi
 
 else
-    error 'Cannot parse script arguments'
-    exit 1
+  error 'Cannot parse script arguments'
+  exit 1
 fi
 
-_RESULT=$(du -sh ${filepath})
-_VALID=$(echo "${_RESULT}" | egrep '0 ' -o)
+_RESULT=$(du -sh "${filepath}")
+_VALID=$(echo "${_RESULT}" | grep -E '^0\s' -o)
 
 # Show error if dump is empty
-if [[ ! -z ${_VALID} ]] ; then
-    error 'Dump failed: file is empty'
-    exit 1
+if [[ -n ${_VALID} ]]; then
+  error 'Dump failed: file is empty'
+  exit 1
 fi
 
 print "Dump is done (${filepath})."
-echo ${_RESULT}
-exit 0
+echo "${_RESULT}"
