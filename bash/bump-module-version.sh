3bfc0a18b8665483a80d856b80942511de2e6b5d
Igor Ivlev <igor.ivlev@verkstedt.com>
2019-11-27 10:39:45 +0100

    Fix bump-ver script, make it work with file in argument

--- bash/bump-module-version.sh
+++ bash/bump-module-version.sh
@@ -13,23 +13,26 @@ if [[ -z "$1" ]]; then
 fi
 
 BASEDIR=$(dirname $0)
+# VERSION_FILE=examples/package.json
+VERSION_FILE=$1
 
 # Include lib
 source ${BASEDIR}/lib.sh
 
-CURRENT_VER=$(get_module_version my-module-header.php)
-
 # Print a version no from the specified file
 function get_module_version() {
-  grep 'Version:' "$1" | sed 's@\s*Version:\s*\([0-9.]\+\).*$@\1@'
+  grep -Ei '\"?Version\"?\s*:' "$1" | sed 's@\s*\"?Version\"?:\s*\"?\([0-9.]\+\).*$@\1@I'
 }
 
-echo "Found version: ${CURRENT_VER}"
-echo "Bump '$1' part ..."
+# Modify this call to add more files which contain version:
+CURRENT_VER=$(get_module_version $VERSION_FILE) 
 
-bumpversion --current-version "${CURRENT_VER}" "$1" my-module-header.php --allow-dirty
+echo "Found version: ${CURRENT_VER}"
+echo "Bump '$2' part ..."
+
+bumpversion --current-version "${CURRENT_VER}" "$2" $VERSION_FILE --allow-dirty
 
 if [[ $? -eq 0 ]]; then
-  echo "Version updated to: $(get_module_version my-module-header.php)"
+  echo "Version updated to: $(get_module_version $VERSION_FILE)"
   exit 0
 fi
